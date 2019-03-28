9f55752b7196228d57091ae88e5c4ff3844c799b
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-28 17:43:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
   min = 0
   min_key = ""
@@ -13,6 +13,7 @@ def key_for_min_value(name_hash)
       min = val
       min_key = key
     end
+    binding.pry
   end
   min_key
 end
